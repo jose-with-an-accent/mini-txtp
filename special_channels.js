4b0c1c4eb24e8bc64e0eb938a382db51edbd7ec3
Jose Sanchez <32964662+josersanchez0117@users.noreply.github.com>
2020-02-25 22:26:50 -0600

    Update special_channels.js

--- special_channels.js
+++ special_channels.js
@@ -1,11 +1,16 @@
 // example uncut: SMG_galaxy04_multi.ast cut: SMG_galaxy04_multi.ast#C3,4.txtp
 const glob = require('glob');
 const fs = require('fs');
-glob("*/*.dspadpcm.bfstm", function (er, files) {
+glob("*/*.bfstm", function (er, files) {
+    // files is an array of filenames.
+    // If the `nonull` option is set, and nothing
+    // was found, then files is ["**/*.js"]
+    // er is an error object or null.
     files.map((name, ind) => {
         console.log("I'm Alive!")
-        const a = Buffer.from(fs.readFileSync(name)).readUInt8(0x62); //get channel info from 0x62
-        for (i = 1; i < a; i = i + 1) {
+        const a = Buffer.from(fs.readFileSync(name)).readUInt8(0x62);
+        for (i = 1; i < a; i = i + 2) {
+
             console.log(`creating ${name}#C${i},${i + 1}.txtp...`, "")
             fs.writeFileSync(`${name}#C${i},${i + 1}.txtp`, "")
         }
