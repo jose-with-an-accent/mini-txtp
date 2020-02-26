cb7f589f99c5e64db1378b9146a510b80586415d
Jose Sanchez <32964662+josersanchez0117@users.noreply.github.com>
2020-02-25 19:47:34 -0600

    hopefully i did it

---
+++ index.js
@@ -0,0 +1,18 @@
+// example uncut: SMG_galaxy04_multi.ast cut: SMG_galaxy04_multi.ast#C3,4.txtp
+const glob = require('glob');
+const fs = require('fs');
+console.log('running script');
+
+glob("music/*", function (er, files) {
+  // files is an array of filenames.
+  // If the `nonull` option is set, and nothing
+  // was found, then files is ["**/*.js"]
+  // er is an error object or null.
+  files.map((name, ind) => {
+    console.log(name)
+    if (name.indexOf('multi') > -1) {
+      fs.writeFileSync(`${name}#C1,2.txtp`, "")
+      fs.writeFileSync(`${name}#C3,4.txtp`, "")
+    }
+  })
+})
